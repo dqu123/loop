73e4d1840be78395651bc8fddb19fb505e2bbefd
David Qu <dqu123.github@gmail.com>
2023-07-05 20:55:07 -0700

    Add external error message

--- backend/controller/handle_games_players.go
+++ backend/controller/handle_games_players.go
@@ -13,9 +13,10 @@ import (
 func handleGamesPlayers(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		httpStatus, err := postPlayer(w, r)
+		httpStatus, externalMessage, err := postPlayer(w, r)
 		if err != nil {
 			w.WriteHeader(httpStatus)
+			io.WriteString(w, externalMessage)
 			logger.LogError("handleGamesPlayers", err)
 		}
 
@@ -31,28 +32,28 @@ type PostPlayerRequest struct {
 	PlayerUUID string
 }
 
-func postPlayer(w http.ResponseWriter, r *http.Request) (int, error) {
+func postPlayer(w http.ResponseWriter, r *http.Request) (int, string, error) {
 	bodyBytes := []byte{}
 	_, err := r.Body.Read(bodyBytes)
 	switch err {
 	case nil:
 		break
 	case io.EOF:
-		return http.StatusBadRequest, errors.New("missing request body")
+		return http.StatusBadRequest, "Error: Request body required", errors.New("missing request body")
 	default:
-		return http.StatusInternalServerError, err
+		return http.StatusInternalServerError, "", err
 	}
 
 	var req PostPlayerRequest
 	err = json.Unmarshal(bodyBytes, &req)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return http.StatusInternalServerError, "", err
 	}
 
 	game, ok := dataGamesMap[req.GameHash]
 	if !ok {
-		return http.StatusInternalServerError, fmt.Errorf("gameHash:%s not found", req.GameHash)
+		return http.StatusInternalServerError, "", fmt.Errorf("gameHash:%s not found", req.GameHash)
 	}
 	game.PlayerMap[req.PlayerUUID] = NewPlayerData()
-	return http.StatusOK, nil
+	return http.StatusOK, "", nil
 }
